04a057d92f7c8fd85d80547bb3609e20e2d88b60
Grzegorz Krukowski <grkrukowski@gmail.com>
2021-11-09 18:27:58 +0100

    v1.2.0 get-paths: use base_sha for changed-files ray: add Raycast Store URL generation when publishing ray: use fixed api_url ray-cli: use `npm CI` instead of `npm install` ray-cli: update validation parameter name to "v" instead of "s" setup-go: update go to version 1.17.3

--- ray-cli/ray_cli.sh
+++ ray-cli/ray_cli.sh
@@ -42,18 +42,20 @@ scheme_path="/tmp/raycast/extensions.json"
 curl https://www.raycast.com/schemas/extension.json --create-dirs -o $scheme_path
 
 starting_dir=$PWD
-ray_full_command="ray $ray_command -s $scheme_path --exitOnError"
+ray_full_command="ray $ray_command -v $scheme_path --exitOnError"
 ray_cli_log_file="/tmp/capture.out"
 
 exit_code=0
+declare -a 'store_urls'
 
 for dir in "${paths[@]}" ; do
     extension_folder=`basename $dir`
     printf "\nEntering $dir\n"
     cd "$dir"
-    npm install
-    printf "Executing '$ray_full_command'\n"
     set +e
+    printf "Executing 'npm CI'\n"
+    npm ci --silent
+    printf "Executing '$ray_full_command'\n"
     $ray_full_command 2>&1 | tee $ray_cli_log_file ; test ${PIPESTATUS[0]} -eq 0
     last_exit_code=${?}
     set -e
@@ -63,9 +65,27 @@ for dir in "${paths[@]}" ; do
     if [ $last_exit_code -ne 0 ]; then
         error_message=`tr '\n' ' ' < $ray_cli_log_file`
         echo "::error title=$command failed for $extension_folder::$error_message"
+    else
+        if [ "$command" == "publish" ]; then
+            author=`cat package.json | jq -r '.author | values'`
+            owner=`cat package.json | jq -r '.owner | values'`
+            name=`cat package.json | jq -r '.name | values'`
+            if [ ! -z "$owner" ]
+            then
+                store_path="$owner/$name"
+            else
+                store_path="$author/$name"
+            fi
+            store_url="https://raycast.com/$store_path"
+            store_urls+=("$store_url")
+        fi
     fi
     cd $starting_dir
 done
 
-
+store_urls_string=$(printf "%s\n" "${store_urls[@]}")
+store_urls_string="${store_urls_string//'%'/'%25'}"
+store_urls_string="${store_urls_string//$'\n'/'%0A'}"
+store_urls_string="${store_urls_string//$'\r'/'%0D'}"
+echo "::set-output name=store_urls::${store_urls_string}"
 exit $exit_code
--- setup-cli/setup_cli.sh
+++ setup-cli/setup_cli.sh
@@ -5,12 +5,10 @@ set -e
 access_token=$1
 api_url=$2
 
-if [ -n "${access_token}" ] && [ -n "${api_url}" ]; then
-    path=~/.config/raycast-debug
-    mkdir -p $path
-    config_file_path=$path/config.json
-    jq -n --arg accesstoken "$access_token" --arg apiurl "$api_url" '{"accesstoken": $accesstoken, "apiurl": $apiurl}' > $config_file_path
-fi
+path=~/.config/raycast-debug
+mkdir -p $path
+config_file_path=$path/config.json
+jq -n --arg accesstoken "$access_token" --arg apiurl "$api_url" '{"accesstoken": $accesstoken, "apiurl": $apiurl}' > $config_file_path
 
 wget -q -O "/usr/local/bin/ray" "https://raycast-cli.s3.eu-central-1.amazonaws.com/linux/ray"
 wait
