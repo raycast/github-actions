284b5c2e322abafcb407a6817d2d6f282e60c88c
LitoMore <litomore@gmail.com>
2024-10-15 17:06:40 +0800

    RayCLI: Fix multiple store URLs

--- ray-cli/ray_cli.sh
+++ ray-cli/ray_cli.sh
@@ -182,8 +182,6 @@ done
 
 # encode multiline output for github actions
 store_urls_string=$(printf "%s\n" "${store_urls[@]}")
-store_urls_string="${store_urls_string//'%'/'%25'}"
-store_urls_string="${store_urls_string//$'\n'/'%0A'}"
-store_urls_string="${store_urls_string//$'\r'/'%0D'}"
+store_urls_string="${store_urls_string//$'\n'/ }"
 echo "store_urls=${store_urls_string}" >> $GITHUB_OUTPUT
 exit $exit_code
